a9fdaf36419b9865bf599c0e456b8cb7f92ed969
raidelacruz <ryuxsein@gmail.com>
2026-07-04 21:07:12 -0500

    cambios_del_backend

--- src/main/java/com/sistema/riskai_backend/dto/EstudianteRiesgoRequest.java
+++ src/main/java/com/sistema/riskai_backend/dto/EstudianteRiesgoRequest.java
@@ -1,5 +1,6 @@
 package com.sistema.riskai_backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Max;
@@ -11,20 +12,26 @@ import lombok.Data;
 public class EstudianteRiesgoRequest {
 
     @NotNull @DecimalMin("0.0") @DecimalMax("20.0")
+    @JsonProperty("promedio_actual")
     private Double promedioActual;
 
     @NotNull @Min(0) @Max(100)
+    @JsonProperty("asistencia_pct")
     private Integer asistenciaPct;
 
     @NotNull @Min(0) @Max(100)
+    @JsonProperty("tareas_entregadas_pct")
     private Integer tareasEntregadasPct;
 
     @NotNull @Min(0) @Max(100)
+    @JsonProperty("participacion_pct")
     private Integer participacionPct;
 
     @NotNull @Min(0) @Max(40)
+    @JsonProperty("horas_estudio_semana")
     private Integer horasEstudioSemana;
 
     @NotNull @DecimalMin("0.0") @DecimalMax("20.0")
+    @JsonProperty("nota_pc_anterior")
     private Double notaPcAnterior;
 }
